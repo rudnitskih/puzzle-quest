f08b927dd19314f773f68ec743b27f69865fbda6
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-11-30 22:11:01 +0200

    Add loading data from spreadsheet

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import {getPieces} from "./getPieces.js";
 
-const canvas = document.querySelector('.canvas');
-let currentZoom = Number(window.getComputedStyle(canvas).zoom);
 
+const canvas = document.querySelector('.canvas');
 canvas.innerHTML = getPieces();
+
+let currentZoom = Number(window.getComputedStyle(canvas).zoom);
+const pieces = [...document.querySelectorAll('.pc')];
+
 
 document.querySelector('.zoom_plus').addEventListener('click', () => {
   updateZoom(0.02);
@@ -17,3 +20,56 @@ function updateZoom(delta) {
   currentZoom += delta;
   canvas.style.zoom = `${currentZoom}`;
 }
+
+function updateVisiblePieces() {
+  const numberColumnName = 'Номера';
+  const isOpenedColumnName = 'Пазл открытый?';
+  const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1u4ycZNkGmTczthSVnijAOzAuZ53-K8Wk76hswaSlViI/edit?usp=sharing';
+
+  return new Promise((resolve) => {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+      callback: (rows) => {
+        function onlyUnique(value, index, self) {
+          return self.indexOf(value) === index;
+        }
+
+        const values = rows
+          .filter((row) => {
+            return !!row[isOpenedColumnName];
+          })
+          .map((row) => row[numberColumnName].split(',').map((stringValue) => stringValue.trim())
+          ).reduce((acc, arrayOfValues) => {
+            acc = acc.concat(arrayOfValues);
+            return acc;
+          }, [])
+          .filter(onlyUnique)
+          .sort((a, b) => a - b);
+
+        console.log(pieces);
+
+        pieces.forEach((piece) => {
+          if (values.includes(piece.textContent.trim())) {
+            piece.classList.add('active');
+          } else {
+            piece.classList.remove('active');
+          }
+        });
+
+
+        resolve();
+      },
+      simpleSheet: true
+    })
+  });
+}
+
+async function infinityUpdateVisiblePuzzles() {
+  await updateVisiblePieces();
+
+  setTimeout(() => {
+    void infinityUpdateVisiblePuzzles();
+  }, 5000)
+}
+
+
+void infinityUpdateVisiblePuzzles();
