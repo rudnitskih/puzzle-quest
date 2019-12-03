7788316810c1d3ba9d83ca0b40f80b8e681e23de
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-12-03 21:22:39 +0200

    Update modal logic

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 import {getPieces} from "./getPieces.js";
 
-
 const canvas = document.querySelector('.canvas');
+const modalNext = document.querySelector('.modal__next');
+const modalInput = document.querySelector('.modal__input');
 canvas.innerHTML = getPieces();
 const isTestMode = location.search.includes('test');
 
 MicroModal.init();
 setZoomListeners();
 
-if (isTestMode) {
-  document.body.classList.add('test');
-} else {
-  document.body.classList.add('prod');
-  void infinityUpdateVisiblePuzzles();
-}
+(async function init() {
+  if (isTestMode) {
+    document.body.classList.add('test');
+  } else {
+    document.body.classList.add('prod');
+    await Promise.all([loadPuzzleData(), getCurrentStep()]);
+
+    updateVisiblePieces();
+    updateTaskModal();
+  }
+})();
+
+
+modalInput.addEventListener('input', (e) => {
+    modalNext.disabled = (e.target.value || '').toLowerCase() !== window.puzzleTasks[window.currentTask].password.toLowerCase();
+});
+
+modalNext.addEventListener('click', () => {
+  setCurrentTask(window.currentTask + 1);
+});
+
 
 function setZoomListeners() {
   let currentZoom = Number(window.getComputedStyle(canvas).zoom);
@@ -33,39 +49,27 @@ function setZoomListeners() {
 }
 
 
-function updateVisiblePieces() {
-  const numberColumnName = 'Номера';
-  const isOpenedColumnName = 'Пазл открытый?';
+function loadPuzzleData() {
   const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1u4ycZNkGmTczthSVnijAOzAuZ53-K8Wk76hswaSlViI/edit?usp=sharing';
-  const pieces = [...document.querySelectorAll('.pc')];
 
   return new Promise((resolve) => {
     Tabletop.init( { key: publicSpreadsheetUrl,
-      callback: (rows) => {
-        function onlyUnique(value, index, self) {
-          return self.indexOf(value) === index;
-        }
+      callback: (data) => {
+        const taskDescriptionColumnName = 'Задание';
+        const taskPasswordColumnName = 'Пароль';
+        const taskNumberName = 'Номер задачи';
 
-        const values = rows
-          .filter((row) => {
-            return !!row[isOpenedColumnName];
-          })
-          .map((row) => row[numberColumnName].split(',').map((stringValue) => stringValue.trim())
-          ).reduce((acc, arrayOfValues) => {
-            acc = acc.concat(arrayOfValues);
-            return acc;
-          }, [])
-          .filter(onlyUnique)
-          .sort((a, b) => a - b);
-
-        pieces.forEach((piece) => {
-          if (values.includes(piece.textContent.trim())) {
-            piece.classList.add('active');
-          } else {
-            piece.classList.remove('active');
+        window.puzzleData = data;
+        window.puzzleTasks = data.reduce((acc, row) => {
+          if (row[taskDescriptionColumnName] && row[taskPasswordColumnName]) {
+            acc[Number(row[taskNumberName])] = {
+              description: row[taskDescriptionColumnName],
+              password: row[taskPasswordColumnName],
+            }
           }
-        });
 
+          return acc;
+        }, []);
 
         resolve();
       },
@@ -74,10 +78,68 @@ function updateVisiblePieces() {
   });
 }
 
-async function infinityUpdateVisiblePuzzles() {
-  await updateVisiblePieces();
+function updateVisiblePieces() {
+  const pieces = [...document.querySelectorAll('.pc')];
+  const numberColumnName = 'Номера';
+  const taskNumber = 'Номер задачи';
 
-  setTimeout(() => {
-    void infinityUpdateVisiblePuzzles();
-  }, 5000)
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  const values = window.puzzleData
+    .filter((row) => {
+      return Number(row[taskNumber]) <= window.currentTask;
+    })
+    .map((row) => row[numberColumnName].split(',').map((stringValue) => stringValue.trim())
+    ).reduce((acc, arrayOfValues) => {
+      acc = acc.concat(arrayOfValues);
+      return acc;
+    }, [])
+    .filter(onlyUnique)
+    .sort((a, b) => a - b);
+
+  pieces.forEach((piece) => {
+    if (values.includes(piece.textContent.trim())) {
+      piece.classList.add('active');
+    } else {
+      piece.classList.remove('active');
+    }
+  });
 }
+
+function updateTaskModal(currentTaskNumber = window.currentTask) {
+  const modalTitle = document.querySelector('.modal__title');
+  const modalDescription = document.querySelector('.modal__task-description');
+
+  modalTitle.innerHTML = `Задание ${currentTaskNumber} из ${window.puzzleTasks.length}`;
+  modalDescription.innerHTML = window.puzzleTasks[currentTaskNumber].description;
+
+  modalNext.disabled = true;
+  modalInput.value = '';
+}
+
+async function getCurrentStep() {
+  const response = await fetch('https://5de6a2cab1ad690014a4dc7c.mockapi.io/tasks/1');
+  const {currentTask} = await response.json();
+
+  window.currentTask = currentTask;
+}
+
+async function setCurrentTask(state) {
+  window.currentTask = state;
+  updateVisiblePieces();
+  updateTaskModal();
+
+  return await fetch('https://5de6a2cab1ad690014a4dc7c.mockapi.io/tasks/1', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      currentTask: state,
+    })
+  });
+}
+
+window.setCurrentTask = setCurrentTask;
